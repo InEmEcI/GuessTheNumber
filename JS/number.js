f76e72020642bf5b0da3f749d1e77c7b61e9839d
InEmEcI <iikar@bk.ru>
2022-09-28 13:22:40 +0300

    ../index.html - поменял ссылку на попробовать ещё раз... на https://inemeci.github.io/GuessTheNumber/

--- JS/number.js
+++ JS/number.js
@@ -67,7 +67,7 @@ function newGame(){
       //document.location.href = 'number.html';
     let div = document.createElement('div');
     /* укажем класс стилей и содержимое:  */
-    div.className = "new_game";
+    div.className = "new_game";    
     div.innerHTML = '<a href="https://inemeci.github.io/GuessTheNumber/>попробовать ещё раз...</a>'; 
     document.body.append(div); //добавим наш div в раздел body
 };
